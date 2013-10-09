122feb2c4a01a9979d053930884a18685d3a34e9
Diego Sapriza <diego@sapriza.net>
2013-10-09 08:00:32 -0300

    tryo no confirmado

--- site/en.php
+++ site/en.php
@@ -449,7 +449,7 @@ info@meetup.uy</a> <br/>
     						<div class="span3 box-sponsors"><a href="http://ingsw.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/ingsw.png"></a></div>
     					</div>
     					<div class="row">
-    						<div class="span3 box-sponsors"><a href="http://www.tryolabs.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/tryo.png"></a></div>
+    						<div class="span3 box-sponsors"><!--<a href="http://www.tryolabs.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/tryo.png"></a>--></div>
     						<div class="span3 box-sponsors"><a href="http://www.codigodelsur.com/website/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/codigo-del-sur.png"></a></div>
     						<div class="span3 box-sponsors"><a href="http://www.wyeworks.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/wyeworks.png"></a></div>
     						<div class="span3 box-sponsors"><a href="http://servergrove.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/servergrove.png"></a></div>
