031112de5c9bbb652ce25f247b2ba5659abb6988
Diego Sapriza <AV4TAr@gmail.com>
2013-10-11 13:09:02 -0200

    velocity

--- site/sponsors.php
+++ site/sponsors.php
@@ -11,9 +11,14 @@
 	<div class="span3 box-sponsors"><a href="http://www.onetree.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/onetree.png"></a></div>
 </div>
 <div class="row">
-
-    <div class="span3 box-sponsors"><a href="http://amaretto.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/amaretto.png"></a></div>
+	<div class="span3 box-sponsors"><a href="http://www.velocitypartners.net/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/velocity-partners.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.pluralsight.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/pluralsight.png"></a></div>
+    <div class="span3 box-sponsors"><a href="http://amaretto.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/amaretto.png"></a></div>
 	<div class="span3 box-sponsors"><a href="#" target="_blank"><img src="./images/sponsors/zillertal.png"></a></div>
+</div>
+<div class="row">
+	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
+	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
+	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
 	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
 </div>
