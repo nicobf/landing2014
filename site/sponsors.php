92f40d5e739ebb03d01e09ce4a5daaec35a1c9de
Diego Sapriza <AV4TAr@gmail.com>
2013-10-16 18:07:47 -0200

    neo y tryo

--- site/sponsors.php
+++ site/sponsors.php
@@ -2,10 +2,10 @@
 	<div class="span3 box-sponsors"><a href="http://case-inc.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/case.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://guruhub.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/guruhub.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.moove-it.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/moove-it.png"></a></div>
-	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
+	<div class="span3 box-sponsors"><a href="http://www.codigodelsur.com/website/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/codigo-del-sur.png"></a></div>
 </div>
 <div class="row">
-	<div class="span3 box-sponsors"><a href="http://www.codigodelsur.com/website/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/codigo-del-sur.png"></a></div>
+	<div class="span3 box-sponsors"><a href="http://www.tryolabs.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/tryo.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.wyeworks.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/wyeworks.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://servergrove.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/servergrove.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.onetree.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/onetree.png"></a></div>
@@ -17,7 +17,7 @@
 	<div class="span3 box-sponsors"><a href="#" target="_blank"><img src="./images/sponsors/zillertal.png"></a></div>
 </div>
 <div class="row">
-	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
+	<div class="span3 box-sponsors"><a href="http://www.neo.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/neo.png"></a></div>
 	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
 	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
 	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
