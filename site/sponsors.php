cd8916676d803c4b915fc475328ba5a3799bcff6
Diego Sapriza <AV4TAr@gmail.com>
2013-10-19 18:54:55 -0200

    uruit

--- site/sponsors.php
+++ site/sponsors.php
@@ -18,7 +18,7 @@
 </div>
 <div class="row">
 	<div class="span3 box-sponsors"><a href="http://www.neo.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/neo.png"></a></div>
-	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
+	<div class="span3 box-sponsors"><a href="http://www.uruit.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/uruit.png"></a></div>
 	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
 	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
 </div>
