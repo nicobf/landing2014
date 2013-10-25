38a32fd34b717631ff3b1ff2ced7cb3e3a629584
Diego Sapriza <AV4TAr@gmail.com>
2013-10-24 23:37:24 -0300

    cuti

--- site/media-sponsors.php
+++ site/media-sponsors.php
@@ -5,4 +5,8 @@
 	<div class="span2 box-sponsors"><a href="http://montevideovalley.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/mvdvalley.png"></a></div>
 	<!-- <div class="span2 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>  -->
 </div>
+<div class="row">
+	<div class="offset2 span2 box-sponsors"><a href="http://www.um.edu.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/cuti.png"></a></div>
+	<div class="span2 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div> 
+</div>
 
