9216915d5e1d6579988bf836ab4797442b6d5168
Diego Sapriza <AV4TAr@gmail.com>
2013-10-24 23:27:03 -0300

    mvdvalley

--- site/media-sponsors.php
+++ site/media-sponsors.php
@@ -2,6 +2,7 @@
 	<div class="offset2 span2 box-sponsors"><a href="http://www.um.edu.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/universidades/um.png"></a></div>
 	<div class="span2 box-sponsors"><a href="http://www.ort.edu.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/universidades/ort.jpg"></a></div>
 	<div class="span2 box-sponsors"><a href="http://www.ucu.edu.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/universidades/ucu.png"></a></div>
-	<div class="span2 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
+	<div class="span2 box-sponsors"><a href="http://montevideovalley.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/mvdvalley.png"></a></div>
+	<!-- <div class="span2 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>  -->
 </div>
 
