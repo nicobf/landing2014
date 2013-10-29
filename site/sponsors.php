9493076320c620e84c65050b6a7af2f83db9a460
Diego Sapriza <AV4TAr@gmail.com>
2013-10-29 16:31:07 -0200

    pepsi

--- site/sponsors.php
+++ site/sponsors.php
@@ -30,7 +30,7 @@
 </div>
 <div class="row">
 	<div class="span3 box-sponsors"><a href="http://www.antel.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/antel.png"></a></div>
-	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
+	<div class="span3 box-sponsors"><a href="http://pepsi.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/pepsi.png"></a></div>
 	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
 	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
 </div>
