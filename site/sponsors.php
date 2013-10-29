228f0a47499e484be2b467702e633e9427e141f9
Diego Sapriza <AV4TAr@gmail.com>
2013-10-29 17:06:48 -0200

    sendgrid

--- site/sponsors.php
+++ site/sponsors.php
@@ -30,7 +30,8 @@
 </div>
 <div class="row">
 	<div class="span3 box-sponsors"><a href="http://genexus.com//?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/genexus.png"></a></div>
+	<div class="span3 box-sponsors"><a href="http://www.sendgrid.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/sendgrid.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.antel.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/antel.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://pepsi.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/pepsi.png"></a></div>
-	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
+	<!--<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>-->
 </div>
