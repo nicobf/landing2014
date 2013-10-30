bd9453e8e77660f3472f28f66af38635cb99fd9a
Diego Sapriza <AV4TAr@gmail.com>
2013-10-29 23:53:44 -0200

    cambio de orden

--- site/sponsors.php
+++ site/sponsors.php
@@ -2,36 +2,41 @@
 	<div class="span3 box-sponsors"><a href="http://case-inc.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/case.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://guruhub.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/guruhub.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.moove-it.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/moove-it.png"></a></div>
-	<div class="span3 box-sponsors"><a href="http://www.codigodelsur.com/website/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/codigo-del-sur.png"></a></div>
+	<div class="span3 box-sponsors"><a href="#" target="_blank"><img src="./images/sponsors/grupo-jordan.png"></a></div>
+	
 </div>
 <div class="row">
+	<div class="span3 box-sponsors"><a href="http://www.codigodelsur.com/website/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/codigo-del-sur.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.tryolabs.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/tryo.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.wyeworks.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/wyeworks.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.servergrove.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/servergrove.png"></a></div>
-	<div class="span3 box-sponsors"><a href="http://www.onetree.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/onetree.png"></a></div>
+	
 </div>
 <div class="row">
+	<div class="span3 box-sponsors"><a href="http://www.onetree.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/onetree.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.velocitypartners.net/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/velocity-partners.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.pluralsight.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/pluralsight.png"></a></div>
-    <div class="span3 box-sponsors"><a href="http://amaretto.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/amaretto.png"></a></div>
-	<div class="span3 box-sponsors"><a href="#" target="_blank"><img src="./images/sponsors/zillertal.png"></a></div>
+    <div class="span3 box-sponsors"><a href="http://www.neo.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/neo.png"></a></div>
+	
 </div>
 <div class="row">
-	<div class="span3 box-sponsors"><a href="http://www.neo.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/neo.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.uruit.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/uruit.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.neonroots.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/neon-roots.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://batangamedia.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/batanga-media.png"></a></div>
+	<div class="span3 box-sponsors"><a href="http://www.mozilla.org/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/mozilla.png"></a></div>
 </div>
 <div class="row">
-	<div class="span3 box-sponsors"><a href="#" target="_blank"><img src="./images/sponsors/grupo-jordan.png"></a></div>
-	<div class="span3 box-sponsors"><a href="http://www.mozilla.org/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/mozilla.png"></a></div>
+
 	<div class="span3 box-sponsors"><a href="http://www.anii.org.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/anii.png"></a></div>
 	<div class="span3 box-sponsors"><a href="https://vividcortex.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/vivid.png"></a></div>
-</div>
-<div class="row">
 	<div class="span3 box-sponsors"><a href="http://genexus.com//?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/genexus.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.sendgrid.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/sendgrid.png"></a></div>
+</div>
+<div class="row">
+	
 	<div class="span3 box-sponsors"><a href="http://www.antel.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/antel.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://pepsi.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/pepsi.png"></a></div>
 	<!--<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>-->
+	<div class="span3 box-sponsors"><a href="http://amaretto.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/amaretto.png"></a></div>
+	<div class="span3 box-sponsors"><a href="#" target="_blank"><img src="./images/sponsors/zillertal.png"></a></div>
 </div>
