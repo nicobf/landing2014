363eacec2ec2a0459e8a66895cbb4a2b44180f0f
Diego Sapriza <AV4TAr@gmail.com>
2013-10-30 00:01:08 -0200

    percona

--- site/sponsors.php
+++ site/sponsors.php
@@ -33,10 +33,15 @@
 	<div class="span3 box-sponsors"><a href="http://www.sendgrid.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/sendgrid.png"></a></div>
 </div>
 <div class="row">
-	
+	<div class="span3 box-sponsors"><a href="http://percona.com/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/percona.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://www.antel.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/antel.png"></a></div>
 	<div class="span3 box-sponsors"><a href="http://pepsi.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/pepsi.png"></a></div>
 	<!--<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>-->
 	<div class="span3 box-sponsors"><a href="http://amaretto.com.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/amaretto.png"></a></div>
+</div>
+<div class="row">
 	<div class="span3 box-sponsors"><a href="#" target="_blank"><img src="./images/sponsors/zillertal.png"></a></div>
+	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
+	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
+	<div class="span3 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>
 </div>
