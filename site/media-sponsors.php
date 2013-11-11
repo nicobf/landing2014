99000abc478f71f1af36e6e3f2943b6e6e2f4866
Diego Sapriza <AV4TAr@gmail.com>
2013-11-11 20:57:52 -0200

    fing

--- site/media-sponsors.php
+++ site/media-sponsors.php
@@ -6,7 +6,8 @@
 	<!-- <div class="span2 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div>  -->
 </div>
 <div class="row">
-	<div class="offset2 span2 box-sponsors"><a href="http://cuti.ort.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/cuti.png"></a></div>
+	<div class="offset2 span2 box-sponsors"><a href="http://cuti.org.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/cuti.png"></a></div>
+	<div class="span2 box-sponsors"><a href="http://www.fing.edu.uy/?ref=tech.meetup.uy" target="_blank"><img src="./images/sponsors/universidades/fing.png"></a></div>
 	<div class="span2 box-sponsors"><a href="mailto:info@meetup.uy?subject=MeetupUy 2013 Apoyo" target="_blank"><img src="./images/sponsors/apoya.png"></a></div> 
 </div>
 
