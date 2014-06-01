5f6901d1664ac1fcbe378647eaa84e6583f15ba3
Santiago Camelo <trikanna@gmail.com>
2014-06-01 20:38:34 -0300

    angular, routes, 2013 layout. styles

--- src/frontend/assets/javascripts/application.js
+++ src/frontend/assets/javascripts/application.js
@@ -23,7 +23,41 @@ $(document).ready(function() {
 *
 * Description
 */
-var techmeetup = angular.module('techmeetup', []);
+var techmeetup = angular.module('techmeetup', ['ngRoute'])
+	.config(function($routeProvider){
+		$routeProvider.
+			when('/', 		{templateUrl : 'templates/2014.html'}).
+			when('/2013', 	{templateUrl : 'templates/2013.html'}).
+			when('/2012', 	{templateUrl : 'templates/2012.html'}).
+			otherwise(      {redirectTo  :'/'} );
+	});
+
+techmeetup.run(function($rootScope, $templateCache) {
+    $rootScope.$on('$routeChangeStart', function(event, next, current) {
+        if (typeof(current) !== 'undefined'){
+            $templateCache.remove(current.templateUrl);
+        }
+    });
+});
+
+techmeetup.controller('main', function($scope, $route, $routeParams, $location){	
+
+	$scope.$on('$viewContentLoaded', function() {
+
+		console.log("HeaderVideo.init()");
+
+		try{
+			HeaderVideo.init({
+		        container     : $('.header-video'),
+		        header        : $('.header-video--media'),
+		        videoTrigger  : $("#video-trigger"),
+		        autoPlayVideo : false
+		    });
+		}catch(e){}
+		
+	});
+
+});
 
 techmeetup.controller('organizers', function ($scope) {
 
