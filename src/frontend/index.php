1a3f457964b80c1da3936bda144e58c7615f8008
Diego Sapriza <diego@sapriza.net>
2014-09-16 22:26:42 -0300

    extensiones

--- src/frontend/index.php
+++ src/frontend/index.php
@@ -22,7 +22,7 @@
         
 	    <div class="container">
 	        <div class="header-menu sixteen columns clearfix">
-	            <a href="index.html" class="three columns alpha logo-container">
+	            <a href="index.php" class="three columns alpha logo-container">
 	                <img src="assets/images/2014/landing/logo.png" alt="">
 	            </a>
 	            <ul class="thirteen columns omega">
